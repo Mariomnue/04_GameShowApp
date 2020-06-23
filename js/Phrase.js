247ba240f93666cd5a3f2724a8825227f02f349b
Mariomnue <mary.pienzi@gmail.com>
2020-06-23 09:05:33 -0400

    Submit

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,53 +1,55 @@
 /* Treehouse FSJS Techdegree
-  * Project 4 - OOP Game App
-  * Phrase.js */
+ * Project 4 - OOP Game App
+ * Phrase.js */
 let letters;
-class Phrase{
-	constructor(phrase){//works
-		this.phrase = phrase.toLowerCase();
-		this.letterCount = 0;
-		this.spaceCount  = 0;
-		this.winCount = 0;
-}
+class Phrase {
+     constructor(phrase) { //works
+          this.phrase = phrase.toLowerCase();
+          this.letterCount = 0;
+          this.spaceCount = 0;
+          this.winCount = 0;
+     }
 
-	addPhraseToDisplay(aPhrase){//works
-		aPhrase = aPhrase;
-console.log("aPhrase: " +aPhrase.phrase);//aPhase is still an object. LEAVE. The other is the phrase. Test the program.
-	const ul = document.getElementById('phrase').firstElementChild;
-	letters = aPhrase.phrase.split('');
-	 	letters.forEach((letter, index) => {
-			let li = document.createElement('li');
-			li.innerHTML = letter;
-			if(letter === " "){
-				li.className =  `space`;
-				this.spaceCount += 1;//account for the spaces
-			}else{li.className = `hide letter ${letter}`};
-			ul.appendChild(li);
-			this.letterCount += 1;
-			if(this.spaceCount === this.wordCount ){
-				this.wordCount += 1;
-			}
-		});
-	}
+     addPhraseToDisplay(aPhrase) { //works
+          aPhrase = aPhrase;
+          console.log("aPhrase: " + aPhrase.phrase); //aPhase is still an object. LEAVE. The other is the phrase. Test the program.
+          const ul = document.getElementById('phrase').firstElementChild;
+          letters = aPhrase.phrase.split('');
+          letters.forEach((letter, index) => {
+               let li = document.createElement('li');
+               li.innerHTML = letter;
+               if (letter === " ") {
+                    li.className = `space`;
+                    this.spaceCount += 1; //account for the spaces
+               } else {
+                    li.className = `hide letter ${letter}`
+               };
+               ul.appendChild(li);
+               this.letterCount += 1;
+               if (this.spaceCount === this.wordCount) {
+                    this.wordCount += 1;
+               }
+          });
+     }
 
 
 
 
-//does the keyup match any letter in the phrase
-//create a boolean, an HTMLCollection with 0 length/which is true, return false.
-//Read more about this in the #unit-04 thread
-	checkLetter(letter){//works
+     //does the keyup match any letter in the phrase
+     //create a boolean, an HTMLCollection with 0 length/which is true, return false.
+     //Read more about this in the #unit-04 thread
+     checkLetter(letter) { //works
           const boolean = document.getElementsByClassName(letter).length === 0;
-              return !boolean;
-	}
+          return !boolean;
+     }
 
-	showMatchedLetter(e, letter){//works
-		letter = e;
-		let chL = document.getElementsByClassName(`hide letter ${letter}`);
-		for(let i=0; i<chL.length; i++){
-			chL[i].classList.add('show');
-			chL[i].classList.remove('hide');
-			this.winCount += 1;
-		}
-	}
+     showMatchedLetter(e, letter) { //works
+          letter = e;
+          let chL = document.getElementsByClassName(`hide letter ${letter}`);
+          for (let i = 0; i < chL.length; i++) {
+               chL[i].classList.add('show');
+               chL[i].classList.remove('hide');
+               this.winCount += 1;
+          }
+     }
 }
--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
  /* Treehouse FSJS Techdegree
   * Project 4 - OOP Game App
   * app.js */
-let newGame;
-const start_btn = document.getElementById('btn__reset').addEventListener('click', (e) =>{
-	if(newGame){
-		newGame.reset();
-	}
-	newGame = new Game();
-	newGame.startGame();
-	document.addEventListener('keyup', (e) =>{
-		newGame.handleInteraction(e.key);//keyboard does not have a class
-		event.preventDefault();
-	})
-	event.preventDefault();
-})
+ let newGame;
+ const start_btn = document.getElementById('btn__reset').addEventListener('click', (e) => {
+      if (newGame) {
+           newGame.reset();
+      }
+      newGame = new Game();
+      newGame.startGame();
+      document.addEventListener('keyup', (e) => {
+           newGame.handleInteraction(e.key); //keyboard does not have a class
+           event.preventDefault();
+      })
+      event.preventDefault();
+ })
 
 
-document.getElementById('qwerty').addEventListener('click', (e) => {
-console.log(e.target.className);
-	if(e.target.className === 'key'){
-		newGame.handleInteraction(e.target.innerText);
-		console.log(e.target.innerText);
-	}
-	event.preventDefault();
-});
+ document.getElementById('qwerty').addEventListener('click', (e) => {
+      console.log(e.target.className);
+      if (e.target.className === 'key') {
+           newGame.handleInteraction(e.target.innerText);
+           console.log(e.target.innerText);
+      }
+      event.preventDefault();
+ });
 
-document.getElementById('qwerty').addEventListener('keyup', (e) =>{
-	if(document.getElementById('overlay').style.display === 'none'){
-		newGame.handleInteraction(e.innerHTML);
-	}
-	event.preventDefault();
-});
+ document.getElementById('qwerty').addEventListener('keyup', (e) => {
+      if (document.getElementById('overlay').style.display === 'none') {
+           newGame.handleInteraction(e.innerHTML);
+      }
+      event.preventDefault();
+ });
